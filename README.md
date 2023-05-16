0ea6286b1d3bb07ec2f7074336184fbb3f7e1a41
Ivo Dias <igd753@gmail.com>
2023-05-16 11:43:15 -0300

    V1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Veracode Mail Report
  
+# Como usar? <br>
+
+# Orientações Gmail:
+Para o caso de utilizar os serviços do Google, precisa gerar uma senha de App <br>
+Mais detalhes em: [Tutorial Senha App](https://stackoverflow.com/questions/66044348/sending-mail-from-powershell-via-gmail-smtp#:~:text=To%20solve%20Powershell%20gmail%20error,google%20account%20using%20below%20steps.&text=After%20login%2C%20go%20to%20https,verification%22%20on%20your%20gamil%20account.) <br>
---
+++ mailReport-Detailed.ps1
@@ -0,0 +1,75 @@
+param (
+    $veracodeAppName
+)
+
+# Bibliotecas funções
+function Send-VeracodeReport {
+    param (
+        $caminhoRelatorio,
+        $To = "mail@dominio.com.br"
+    )
+
+    try {
+        # Credencial
+        $contaEmail = "mail@dominio.com.br"
+        $Password = ConvertTo-SecureString "senhadeApp" -AsPlainText -Force
+        $credencialMail = New-Object System.Management.Automation.PSCredential ($contaEmail, $Password)
+
+        # Com base no nome do arquivo recebe os dados do App e o Build
+        $stringInfo = $caminhoRelatorio.Split("\App-")[1]
+        $veracodeAppName = $stringInfo.Split("-")[0]
+        $BuildVersion = $stringInfo.Split("-")[1] -replace (".pdf", "")
+
+        # Envia o e-mail
+        Send-MailMessage `
+            -To $To `
+            -Subject "Veracode Report - App $veracodeAppName - Build: $BuildVersion" `
+            -Body "Segue em anexo o relatorio detalhado no ultimo scan do perfil $veracodeAppName" `
+            -BodyAsHtml `
+            -Priority high `
+            -UseSsl `
+            -Port 587 `
+            -SmtpServer 'smtp.gmail.com' `
+            -From $contaEmail `
+            -Credential $credencialMail `
+            -Attachments $caminhoRelatorio
+        Write-Host "Enviado o relatorio: App $veracodeAppName - Build: $BuildVersion"
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message # Recebe o erro
+        Write-Host "Erro ao enviar o relatorio:"
+        Write-Host "$ErrorMessage"
+    }
+    
+}
+
+function Get-VeracodeReport {
+    param (
+        $veracodeAppName
+    )
+    
+    # Recebe o App ID com base no nome da aplicacao dentro do Veracode
+    [xml]$INFO = $(VeracodeAPI.exe -action GetAppList | Select-String -Pattern $veracodeAppName)
+    # Filtra o App ID
+    $appID = $INFO.app.app_id
+
+    try {
+        # Pega o ID da build
+        [xml]$buildINFO = $(VeracodeAPI.exe -action getbuildinfo -appid $appID)
+        $buildID = $buildINFO.buildinfo.build_id
+        $BuildVersion = $buildINFO.buildinfo.build.version
+        $caminhoRelatorio = "$env:LOCALAPPDATA\App-$veracodeAppName-$BuildVersion.pdf"
+        # Gera o relatorio
+        $out = VeracodeAPI.exe -action DetailedReport -buildid "$buildID" -format pdf -outputfilepath "$caminhoRelatorio"
+        return $caminhoRelatorio
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message # Recebe o erro
+        Write-Host "Erro ao gerar o relatorio:"
+        Write-Host "$ErrorMessage"
+    }
+}
+
+# Faz o processo de envio
+$caminhoRelatorio = Get-VeracodeReport $veracodeAppName
+Send-VeracodeReport $caminhoRelatorio
